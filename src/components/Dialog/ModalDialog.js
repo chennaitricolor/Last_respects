50413743da30a8d8c6b895141a196ea5c20304be
barani100 <32703295+barani100@users.noreply.github.com>
2021-06-11 19:07:07 +0530

    Task/reschedule changes (#13)
    * re-assign changes
    
    * updated reassign changes
    
    * updated code changes
    
    * updated code changes
    
    Co-authored-by: Baranikumar D <Baranikumar.D@mrcooper.com>

--- src/components/Dialog/ModalDialog.js
+++ src/components/Dialog/ModalDialog.js
@@ -131,7 +131,7 @@ const ModalDialog = (props) => {
   let date = new Date();
   const [selectedDate, setSelectedDate] = useState(null);
   const [selectedTime, setSelectedTime] = useState('');
-  const [maxDate, setMaxDate] = useState(date.setDate(new Date().getDate() + 1));
+  const [maxDate] = useState(date.setDate(new Date().getDate() + 1));
   const [showError, setShowError] = useState(false);
   const [enableSubmit, setEnableSubmit] = useState(false);
   const [availableTimeSlots, setAvailableTimeSlots] = useState([]);
@@ -144,7 +144,8 @@ const ModalDialog = (props) => {
   const isActive = payload.isActive;
   const isOwner = payload.isOwner;
   const siteId = payload.siteId;
-
+  let reAssignSiteId = 0;
+  
   useEffect(() => {
     if (availableSlotDetails !== null) {
       let formattedDate = moment(selectedDate).format('DD-MM-YYYY');
@@ -200,6 +201,7 @@ const ModalDialog = (props) => {
       const siteFilterCdn = siteList.filter((site) => site.site_name === siteDetails.siteName);
       if (siteFilterCdn.length > 0) {
         let siteId = siteFilterCdn[0].id;
+        reAssignSiteId = siteId;
         dispatch({
           type: actionTypes.GET_SLOTS_BASED_SITE_ID,
           payload: {
@@ -219,8 +221,12 @@ const ModalDialog = (props) => {
       selectedDate !== '' &&
       selectedTime !== '' &&
       selectedReason !== '' &&
-      isOwner;
-
+      isOwner ;
+    
+    if(!isActive){
+      result = true; 
+    }
+    
     result = showReAssignComment ? commentVal !== '' : result;
 
     result ? setEnableSubmit(true) : setEnableSubmit(false);
@@ -277,23 +283,25 @@ const ModalDialog = (props) => {
     if (token !== '' && isTokenAlive(token)) {
       let api = apiUrls.updateSlotStatus.replace(':slotId', props.slotDetails.id);
       let slotDetails = props.slotDetails;
+      const finalSiteId =  isActive ? parseInt(siteId) : parseInt(reAssignSiteId) ;
+      slotDetails.burialSiteId = finalSiteId;
+
       slotDetails.id = undefined;
       slotDetails.slot = selectedTime;
       slotDetails.dateOfCremation = moment(selectedDate, 'DD-MM-YYYY').format('MM-DD-YYYY');
-      slotDetails.burialSiteId = parseInt(siteId);
 
-      let reAssignPayload = {
+      let putPayload = {
         slotDetails: slotDetails,
         type: 'REASSIGN',
         reason: showReAssignComment ? selectedReason + ' - ' + commentVal : selectedReason,
       };
 
-      callFetchApi(api, null, 'PUT', reAssignPayload, token).then((response) => {
+      callFetchApi(api, null, 'PUT', putPayload, token).then((response) => {
         if (response.status === 200) {
           dispatch({
             type: actionTypes.GET_SLOTS_BASED_SITE_ID,
             payload: {
-              siteId: siteId,
+              siteId: finalSiteId,
             },
           });
           let formattedDate = moment(selectedDate).format('MMM-DD');
@@ -387,9 +395,6 @@ const ModalDialog = (props) => {
                   value={selectedDate}
                   maxDate={maxDate}
                   onChange={handleDateChange}
-                  KeyboardButtonProps={{
-                    'aria-label': 'change date',
-                  }}
                 />
               </MuiPickersUtilsProvider>
             </div>
