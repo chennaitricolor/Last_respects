9c0eedec0dc86a05593c9783dd0e796afc337a67
KArtHiK <karthiknatarajan04@gmail.com>
2021-06-11 15:31:01 +0530

    Machinery management back end integration (#11)
    * Update switch status to backend on switch change
    
    * BE integration for machinery downtime audit
    
    * Prepopulate Site on navigating from machinery

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,10 +1,12 @@
 export const apiUrls = {
   login: '/api/users/login',
   allZones: '/api/zones',
-  postSlot:'/api/slots',
-  updateSlotStatus:'/api/slots/:slotId',
+  postSlot: '/api/slots',
+  updateSlotStatus: '/api/slots/:slotId',
+  updateSiteStatus: '/api/sites/:siteId',
   getSitesBasedOnZoneId: '/api/zones/:zoneId/sites',
   getSlotsBasedOnSiteId: '/api/sites/:siteId/slots',
   getSlotDetailsBasedOnSlotId: '/api/slots/:slotId',
   getAvailableSlotDetailsBasedOnSiteId: '/api/sites/:siteId/slots?date=:date&onlyFreeSlots=:availableFlag',
+  getDowntimeAuditOnSlotId: '/api/machineryDowntimeAudit/:siteId',
 };
